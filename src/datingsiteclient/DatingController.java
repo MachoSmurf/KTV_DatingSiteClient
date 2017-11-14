efe96ff94b154114edcc7dbb183684045217fc67
Raymond <mr_me_9@hotmail.com>
2017-11-14 09:37:27 +0100

    DatingController implementation

--- src/datingsiteclient/DatingController.java
+++ src/datingsiteclient/DatingController.java
@@ -60,5 +60,129 @@ public class DatingController {
         }
     }
     
+    public String GetSiteName(){
+        return siteName;
+    }    
+    
+    public ParticipantInfo GetMyInfo(){
+        if (sessionKey.length() == 10){
+            try{
+                this.myParticipantInfo = hm.getParticipantInfo(sessionKey);
+                return this.myParticipantInfo;
+            }
+            catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+                return null;
+            }
+        }
+        return null;
+    }
+    
+    public Profile GetMyProfile(){
+        if (sessionKey.length() == 10){
+            try{
+                this.myProfile = hm.getProfile(sessionKey);
+                return this.myProfile;
+            }
+            catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+                return null;
+            }
+        }
+        return null;
+    }
+    
+    public Preference GetMyPreference(){
+        if (sessionKey.length() == 10){
+            try{
+                this.myPreference = hm.getPreference(sessionKey);
+                return this.myPreference;
+            }
+            catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+                return null;
+            }
+        }
+        return null;
+    }
+    
+    public List<Profile> GetMatches(){
+        ReloadMatches();
+        return this.myMatches;
+    }
+    
+    private void ReloadMatches(){
+        if (sessionKey.length() == 10){
+            try{
+                this.myMatches = hm.requestMatchingProfiles(sessionKey);
+            }
+            catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+    
+    public List<Message> GetSendMessages(){
+        if (sessionKey.length() == 10){
+            try{
+                this.messagesSend = hm.getMessagesSent(sessionKey);
+                return this.messagesSend;
+            }
+            catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+                return null;
+            }
+        }
+        return null;
+    }
+    
+    public List<Message> GetReceivedMessages(){
+        if (sessionKey.length() == 10){
+            try{
+                this.messagesReceived = hm.getMessagesReceived(sessionKey);
+                return this.messagesReceived;
+            }
+            catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+                return null;
+            }
+        }
+        return null;
+    }
+    
+    public boolean SetMyProfile(int length, ColorHair hairColor, ColorEyes eyeColor, String hobbies, String description){
+        
+       if ((length>100)  && (length<250) && (hairColor != null) && (eyeColor != null) && (!hobbies.isEmpty()) && (!description.isEmpty())){
+           try{
+               this.myProfile = hm.setProfile(sessionKey, length, hairColor, eyeColor, hobbies, description);
+               return true;
+           }
+           catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+                return false;
+            }
+       }
+       System.out.println("Use correct parameters when settings profile");
+       return false;
+    }
+    
+    public boolean SetMyPreferences(Gender gender, int minAge, int maxAge, int minLength, int maxLength, ColorHair hairColor, ColorEyes eyeColor){
+        if ((gender != null) && (minAge>18)  && (maxAge<130) && (hairColor != null) && (eyeColor != null) && (minAge>100)  && (maxAge<250)){
+           try{
+               this.myPreference = hm.setPreference(sessionKey, gender, minAge, maxAge, minLength, maxLength, hairColor, eyeColor);
+               return true;
+           }
+           catch(DatingSiteWebServiceException_Exception e){
+                System.out.println(e);
+                return false;
+            }
+       }
+       System.out.println("Use correct parameters when settings profile");
+       return false;
+    }
+       
+    public boolean SendMessage(){
+        return false;
+    }
     
 }
